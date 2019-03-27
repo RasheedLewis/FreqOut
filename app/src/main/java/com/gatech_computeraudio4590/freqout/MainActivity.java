7fadf69dc1fb41a2c6cb5c6717c7d886d5e404b3
jihanko42 <jihanko42@gmail.com>
2019-03-26 23:40:47 -0400

    Camera Permission asked upon opening app.

--- app/src/main/java/com/gatech_computeraudio4590/freqout/MainActivity.java
+++ app/src/main/java/com/gatech_computeraudio4590/freqout/MainActivity.java
@@ -1,10 +1,21 @@
 package com.gatech_computeraudio4590.freqout;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
 import android.media.MediaPlayer;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import java.io.IOException;
 
@@ -14,13 +25,41 @@ public class MainActivity extends AppCompatActivity  {
     private final int[] mResIds = {R.raw.anger, R.raw.contempt, R.raw.disgust, R.raw.happiness,
         R.raw.neutral, R.raw.sadness};
 
+    private static final int CAMERA_REQUEST_CODE = 100;
+
     private MediaPlayer mMediaPlayer;
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+            requestPermissions(new String[] {Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+        }
+
+
+        Button btnCamera = (Button)findViewById(R.id.btnCamera);
+        imageView = (ImageView)findViewById(R.id.imageView);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
+    }
+
 
     private void playSound(final EmotionType emotionType) {
         final int resId = mResIds[emotionType.ordinal()];
