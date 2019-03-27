a419768f5ab0b6b55d5c4b914225e5411f105205
Rasheed Lewis <rlewis67@gatech.edu>
2019-03-26 22:39:26 -0400

    Added sounds to the Android application
    There is now an `EmotionType` enum that maps to the correct sound within
    a new raw folder under the res package. The `MainActivity` now contains
    a `playSound(EmotionType)` method that plays the correct sound.

---
+++ app/src/main/java/com/gatech_computeraudio4590/freqout/EmotionType.java
@@ -0,0 +1,10 @@
+package com.gatech_computeraudio4590.freqout;
+
+public enum EmotionType {
+    ANGER,
+    CONTEMPT,
+    DISGUST,
+    HAPPINESS,
+    NEUTRAL,
+    SADNESS
+}
--- app/src/main/java/com/gatech_computeraudio4590/freqout/MainActivity.java
+++ app/src/main/java/com/gatech_computeraudio4590/freqout/MainActivity.java
@@ -1,13 +1,42 @@
 package com.gatech_computeraudio4590.freqout;
 
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity  {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private final int[] mResIds = {R.raw.anger, R.raw.contempt, R.raw.disgust, R.raw.happiness,
+        R.raw.neutral, R.raw.sadness};
+
+    private MediaPlayer mMediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void playSound(final EmotionType emotionType) {
+        final int resId = mResIds[emotionType.ordinal()];
+
+        if (mMediaPlayer == null) {
+            mMediaPlayer = MediaPlayer.create(this, resId);
+        } else {
+            AssetFileDescriptor assetFileDescriptor = this.getResources().openRawResourceFd(resId);
+            try {
+                mMediaPlayer.setDataSource(assetFileDescriptor);
+            } catch (IOException e) {
+                Log.e(TAG, e.getMessage());
+            }
+        }
+        if (!mMediaPlayer.isPlaying()) {
+            mMediaPlayer.start();
+        }
     }
 }
